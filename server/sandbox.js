ed6522462aa057c187d1a5344a9f03d12d4e4878
Alexandre Smadja <alexandre.smadja0401@gmail.com>
2023-03-13 19:16:23 +0100

    scrap all eshops

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,12 +1,19 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
-const monlimartbrand = require('./eshops/montlimartbrand')
+const montlimartbrand = require('./eshops/montlimartbrand');
+const circlebrand = require('./eshops/circlebrand');
 
-async function sandbox (eshop = 'https://www.montlimart.com/99-vetements') {
+const dedicated = ['dedicated', "https://www.dedicatedbrand.com/en/men/t-shirts"];
+const montlimard = ['montlimard', "https://www.montlimart.com/99-vetements"];
+const circle = ['circle', "https://shop.circlesportswear.com/collections/collection-homme"];
+
+const eshops = [dedicated,montlimard /*,circle*/];
+
+async function sandbox (eshop = 'https://shop.circlesportswear.com/collections/collection-homme') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
-    const products = await monlimartbrand.scrape(eshop);
+    const products = await circlebrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
