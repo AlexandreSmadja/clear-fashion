4be2f0207ca3d38a48a2effaa7989be861a4978b
Alexandre Smadja <alexandre.smadja0401@gmail.com>
2023-03-21 18:23:21 +0100

    query mongoDB sort by date

--- server/api.js
+++ server/api.js
@@ -1,6 +1,12 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const {ObjectId} = require('mongodb')
+
+const {MongoClient} = require('mongodb');
+
+const MONGODB_DB_NAME = 'clear-fashion';
+const MONGODB_URI = 'mongodb+srv://asmadja:EAveVGPvYlYepK8g@clear-fashion.wwjougk.mongodb.net/test?retryWrites=true&writeConcern=majority';
 
 const PORT = 8092;
 
@@ -8,16 +14,49 @@ const app = express();
 
 module.exports = app;
 
+
+
 app.use(require('body-parser').json());
 app.use(cors());
 app.use(helmet());
 
 app.options('*', cors());
 
+async function getProduct() {
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db = client.db(MONGODB_DB_NAME);
+  const collection = db.collection('products');
+
+  app.get('/products/:idProd', async (request, response) => {
+    try {
+      const {idProd} = request.params;
+      const result = await collection.findOne({_id:ObjectId(idProd)});
+      
+      response.send(result)
+    }
+    catch(err) {
+      console.error(err.message);
+    }    
+  });
+
+  app.get('/products/search/', async (request, response) => {
+    try {
+
+    }
+    catch(err) {
+      console.error(err.message);
+    }
+  })
+}
+
+
+
 app.get('/', (request, response) => {
   response.send({'ack': true});
 });
 
+
 app.listen(PORT);
 
 console.log(`📡 Running on port ${PORT}`);
+getProduct();
